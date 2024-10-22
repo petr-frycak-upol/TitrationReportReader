fda560f24996170b0f13502d9668b0dedc0ae02a
petr-frycak-upol <petr.frycak@upol.cz>
2024-10-22 09:42:28 +0200

    wrong_answer variable --> acceptable_answer

--- trr.py
+++ trr.py
@@ -1,19 +1,17 @@
 import trr_fileio
 
-
-lines = []
 titration_data = []
-report_file = "d:\\python_projects\\report.rpt"
+report_file = "D:\\python_projects\\report.rpt"
 temp_file = "D:\\python_projects\\report_extract.txt"
+VOLUME_POS = 1
+PH_POS = 3
 
 def process_line(line):
     s = line.split()
-    titration_data.append((float(s[1]), float(s[3])))
+    titration_data.append((float(s[VOLUME_POS]), float(s[PH_POS])))
     
 
-
-
-lines = trr_fileio.read_file(report_file)       
+lines = trr_fileio.read_file(report_file)
 
 line_interesting = False
 
--- trr_fileio.py
+++ trr_fileio.py
@@ -1,28 +1,78 @@
 import os.path
 
 def write_file(data, fullname):
+    final_fullname = ""
+    folder = os.path.split(fullname)[0]
+    filename = os.path.split(fullname)[1]
+    rootname = os.path.splitext(filename)[0]
+    extension = os.path.splitext(filename)[1]
+
     if os.path.exists(fullname):
         acceptable_answer = False
+
         while not acceptable_answer:
-            answer = input("File exists! Overwrite (O), Rename manually (R), Append number (A), Cancel (C)\n")
+
+            answer = input(f"File {filename} exists! Overwrite (O), Rename manually (R), Append number (A), Cancel (C):\n")
 
             if answer.upper() == "O":
-                wrong_answer = False
-            elif answer.upper() == "R":
-                wrong_answer = False
-            elif answer.upper() == "A":
-                wrong_answer = False
-            elif answer.upper() == "C":
-                wrong_answer = False
+                final_fullname = fullname
+                acceptable_answer = True
 
-    with open(fullname, mode="w") as f:
+            elif answer.upper() == "R":
+                newname = input("Enter new file name (do not include full path and extension):\n")
+                newname += extension
+                final_fullname = os.path.join((os.path.split(fullname))[0], newname)
+                if not os.path.exists(final_fullname): acceptable_answer = True
+
+            elif answer.upper() == "A":
+                final_fullname = provide_valid_filename(folder, rootname, extension)
+                acceptable_answer = True
+
+            elif answer.upper() == "C":
+                acceptable_answer = True
+                print("Cancelled, file not written.")
+                return
+
+    with open(final_fullname, mode="w") as f:
         for (volume, ph) in data:
             f.write(str(volume))
             f.write(",")
             f.write(str(ph))
             f.write("\n")
+    print(f"File written to {final_fullname}")
         
-    
+def provide_valid_filename(folder, rootname, extension):
+    is_zero_to_nine = True
+    is_valid_filename = False
+    ZERO_TO_NINE = "0123456789"
+
+    underscore_rindex = rootname.rfind("_")
+
+    root_head = ""
+    root_tail_number = 0
+
+    if underscore_rindex == -1:
+        root_head = rootname + "_"
+    elif underscore_rindex == len(rootname)-1: #tj. jméno končí podtržítkem
+        root_head = rootname
+    else:
+        root_tail = rootname[underscore_rindex + 1:]
+        root_head = rootname[:underscore_rindex - 1]
+        for char in root_tail:
+            if not char in ZERO_TO_NINE: is_zero_to_nine = False
+        if is_zero_to_nine:
+            root_tail_number = int(root_tail)
+        else:
+            root_head = rootname + "_"
+
+    new_root_tail = str(root_tail_number)
+    valid_filename = os.path.join(folder, root_head + new_root_tail + extension)
+    while os.path.exists(valid_filename):
+        root_tail_number += 1
+        new_root_tail = str(root_tail_number)
+        valid_filename = os.path.join(folder, root_head + new_root_tail + extension)
+
+    return valid_filename
 
 def read_file(fullname):
     lines = []
