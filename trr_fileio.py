1488914ff0b43d6e171127c3c28ae5b76a87a2e4
Jakub-Koza <kuba.koza.5352@seznam.cz>
2024-10-29 15:53:23 +0100

    Druhá derivace

--- trr_fileio.py
+++ trr_fileio.py
@@ -79,6 +79,15 @@ def provide_valid_filename(folder, rootname, extension):
 
     return valid_filename
 
+def CalcSeconder(data):
+    output=[]
+    for i in range(len(data)-1):
+        (volume0, derivation0) = data[i]
+        (volume1, derivation1) = data[i+1] 
+        x = ((derivation1-derivation0)/(volume1-volume0))
+        output.append(((volume1-volume0)/2, x))
+        return output
+
 def read_file(fullname):
     lines = []
     with open(fullname, "rt", encoding="ansi") as f:
