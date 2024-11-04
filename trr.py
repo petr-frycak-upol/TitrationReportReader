9d7d0a65339c6bbbd3ef79cb4ef9c34894bae540
petr-frycak-upol <petr.frycak@upol.cz>
2024-11-04 23:04:06 +0100

    přesun kódu modulu na konec souboru

--- trr.py
+++ trr.py
@@ -35,6 +35,18 @@ def CalcFirstDer(data):
     return output
 
 
+def find_eq_point(sec_derivative):
+    for i in range(len(sec_derivative) - 1):
+        # vezme vždy druhou hodnotu v daném a za ním následujícím tuplu, vynásobí a pokud je hodnota záporná, došlo k překročení osy x
+        if sec_derivative[i][1] * sec_derivative[i + 1][1] < 0:
+            break
+    x1, y1 = sec_derivative[i]
+    x2, y2 = sec_derivative[i + 1]
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return -b / m
+
+
 lines = trr_fileio.read_file(report_file)
 
 line_interesting = False
@@ -53,15 +65,5 @@ for line in lines:
 print(titration_data)
 trr_fileio.write_file(titration_data, temp_file)
 
-def find_eq_point(sec_derivative):
-    for i in range(len(sec_derivative) - 1):
-        # vezme vždy druhou hodnotu v daném a za ním následujícím tuplu, vynásobí a pokud je hodnota záporná, došlo k překročení osy x
-        if sec_derivative[i][1] * sec_derivative[i + 1][1] < 0:
-            break
-    x1, y1 = sec_derivative[i]
-    x2, y2 = sec_derivative[i + 1]
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
-    extrap = -b / m
-    return extrap
+
 
