aeda65dc7486d624276d0ed01291f83114aa4d01
mlcoma05 <mmlcoch14@gmail.com>
2024-11-04 23:00:40 +0100

    Mm (#5)
    * Bod ekvivalence
    
    * Funkce najde bod ekvivalence z druhé derivace
    
    ---------
    
    Co-authored-by: petr-frycak-upol <petr.frycak@upol.cz>

--- trr.py
+++ trr.py
@@ -6,10 +6,11 @@ temp_file = "D:\\python_projects\\report_extract.txt"
 VOLUME_INDEX = 1
 PH_INDEX = 3
 
+
 def process_line(line):
     s = line.split()
     titration_data.append((float(s[VOLUME_INDEX]), float(s[PH_INDEX])))
-    
+
 
 
 
@@ -40,16 +41,27 @@ line_interesting = False
 
 for line in lines:
     if line_interesting:
-        if line == "": line_interesting = False
+        if line == "":
+            line_interesting = False
         else:
             process_line(line)
     else:
         if ("Volume" in line and "pH" in line):
             line_interesting = True
-            #print(line.split())
+            # print(line.split())
 
 print(titration_data)
 trr_fileio.write_file(titration_data, temp_file)
 
-print("")
+def find_eq_point(sec_derivative):
+    for i in range(len(sec_derivative) - 1):
+        # vezme vždy druhou hodnotu v daném a za ním následujícím tuplu, vynásobí a pokud je hodnota záporná, došlo k překročení osy x
+        if sec_derivative[i][1] * sec_derivative[i + 1][1] < 0:
+            break
+    x1, y1 = sec_derivative[i]
+    x2, y2 = sec_derivative[i + 1]
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    extrap = -b / m
+    return extrap
 
