e4ef417da6c2ea7773022a34e9164d2e999b789f
Jakub-Koza <kuba.koza.5352@seznam.cz>
2024-11-14 11:16:11 +0100

    Druhá derivace a výpočet bodu ekvivalence

--- trr.py
+++ trr.py
@@ -1,19 +1,17 @@
 import trr_fileio
 
-titration_data = []
-report_file = "D:\\python_projects\\report.rpt"
-temp_file = "D:\\python_projects\\report_extract.txt"
+if __name__ != '__main__': print("Import of trr successful")
+report_file = "C:\\Users\\kubak\\Desktop\\Škola\\Navazující\\1.ZS\\PNAC\\Projekty\\report_extract.txt"
+temp_file = "C:\\Users\\kubak\\Desktop\\Škola\\Navazující\\1.ZS\\PNAC\\Projekty\\report.RPT"
 VOLUME_INDEX = 1
 PH_INDEX = 3
 
 
-def process_line(line):
+def process_line(line, titration_data):
     s = line.split()
     titration_data.append((float(s[VOLUME_INDEX]), float(s[PH_INDEX])))
 
-
-
-def Calc_n_Der(data, n):
+def calc_n_der(data, n = 1):
     if n == 0:
         return data
 
@@ -25,26 +23,7 @@ def Calc_n_Der(data, n):
         x = (ph1 - ph0) / (volume1 - volume0)
         output.append(((volume1 + volume0) / 2, x))
 
-    return Calc_n_Der(output, n-1)
-
-
-lines = trr_fileio.read_file(report_file)
-
-line_interesting = False
-
-for line in lines:
-    if line_interesting:
-        if line == "":
-            line_interesting = False
-        else:
-            process_line(line)
-    else:
-        if ("Volume" in line and "pH" in line):
-            line_interesting = True
-            # print(line.split())
-
-print(titration_data)
-trr_fileio.write_file(titration_data, temp_file)
+    return calc_n_der(output, n - 1)
 
 def min_max_index(data):
     min_index, max_index = 0, 0
@@ -55,12 +34,12 @@ def min_max_index(data):
             max_index = i
     return min_index, max_index
 
-
 def find_eq_point(sec_derivative):
     (min_index, max_index) = min_max_index(sec_derivative)
     data = sec_derivative[min_index:max_index]
     for i in range(len(data) - 1):
-        # vezme vždy druhou hodnotu v daném a za ním následujícím tuplu, vynásobí a pokud je hodnota záporná, došlo k překročení osy x
+        # vezme vždy druhou hodnotu v daném a za ním následujícím tuplu, vynásobí a pokud je hodnota záporná,
+        # došlo k překročení osy x
         if data[i][1] * data[i + 1][1] < 0:
             break
     x1, y1 = data[i]
@@ -69,3 +48,33 @@ def find_eq_point(sec_derivative):
     b = y1 - m * x1
     extrap = -b / m
     return extrap
+
+def get_titration_curve(report_file):
+    titration_data = []
+    lines = trr_fileio.read_file(report_file)
+
+    line_interesting = False
+
+    for line in lines:
+        if line_interesting:
+            if line == "":
+                line_interesting = False
+            else:
+                process_line(line, titration_data)
+        else:
+            if ("Volume" in line and "pH" in line):
+                line_interesting = True
+                # print(line.split())
+
+    return titration_data
+    #print(titration_data)
+    #trr_fileio.write_file(titration_data, temp_file)
+
+def secder_eqpoint(data):
+    parameters = []
+    titration_curve = get_titration_curve(data) # vytvoření titrační křivky
+    sec_derivation = calc_n_der(titration_curve, 2) # výpočet druhé derivace
+    eq_point = find_eq_point(sec_derivation) # výpočet bodu titrace
+    parameters.append([titration_curve, sec_derivation, eq_point]) # vytvoření seznamu trojic hodnot
+    
+    return parameters
